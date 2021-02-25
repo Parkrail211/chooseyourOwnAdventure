b1f0dda7fd433e5819abfada9d7122051c776166
Parker Railton <parkrail211@outlook.com>
2021-02-25 12:24:43 -0500

    Add project files.

---
+++ chooseyourOwnAdventure/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿
+namespace chooseyourOwnAdventure
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.choiceBlue = new System.Windows.Forms.Label();
+            this.choiceYellow = new System.Windows.Forms.Label();
+            this.choiceRed = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.outputLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.outputLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(83, 248);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(751, 40);
+            this.outputLabel.TabIndex = 0;
+            this.outputLabel.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::chooseyourOwnAdventure.Properties.Resources._436abb31c4c2bad27e9bc22b5ca318cf__2_;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(83, 332);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(81, 38);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::chooseyourOwnAdventure.Properties.Resources._436abb31c4c2bad27e9bc22b5ca318cf__1_;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(122, 373);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(81, 38);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = global::chooseyourOwnAdventure.Properties.Resources._436abb31c4c2bad27e9bc22b5ca318cf1;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(84, 415);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(79, 39);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImage = global::chooseyourOwnAdventure.Properties.Resources._436abb31c4c2bad27e9bc22b5ca318cf__3_1;
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox4.Location = new System.Drawing.Point(747, 82);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(87, 43);
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackgroundImage = global::chooseyourOwnAdventure.Properties.Resources._436abb31c4c2bad27e9bc22b5ca318cf__3_1;
+            this.pictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox5.Location = new System.Drawing.Point(747, 165);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(87, 43);
+            this.pictureBox5.TabIndex = 6;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackgroundImage = global::chooseyourOwnAdventure.Properties.Resources._436abb31c4c2bad27e9bc22b5ca318cf__3_1;
+            this.pictureBox6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox6.Location = new System.Drawing.Point(83, 165);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(87, 43);
+            this.pictureBox6.TabIndex = 7;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackgroundImage = global::chooseyourOwnAdventure.Properties.Resources._436abb31c4c2bad27e9bc22b5ca318cf__3_1;
+            this.pictureBox7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox7.Location = new System.Drawing.Point(84, 82);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(87, 43);
+            this.pictureBox7.TabIndex = 8;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Location = new System.Drawing.Point(122, 41);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(665, 207);
+            this.pictureBox8.TabIndex = 9;
+            this.pictureBox8.TabStop = false;
+            // 
+            // choiceBlue
+            // 
+            this.choiceBlue.BackColor = System.Drawing.Color.Black;
+            this.choiceBlue.ForeColor = System.Drawing.Color.White;
+            this.choiceBlue.Location = new System.Drawing.Point(203, 345);
+            this.choiceBlue.Name = "choiceBlue";
+            this.choiceBlue.Size = new System.Drawing.Size(250, 13);
+            this.choiceBlue.TabIndex = 10;
+            // 
+            // choiceYellow
+            // 
+            this.choiceYellow.BackColor = System.Drawing.Color.Black;
+            this.choiceYellow.ForeColor = System.Drawing.Color.White;
+            this.choiceYellow.Location = new System.Drawing.Point(241, 384);
+            this.choiceYellow.Name = "choiceYellow";
+            this.choiceYellow.Size = new System.Drawing.Size(250, 13);
+            this.choiceYellow.TabIndex = 11;
+            // 
+            // choiceRed
+            // 
+            this.choiceRed.BackColor = System.Drawing.Color.Black;
+            this.choiceRed.ForeColor = System.Drawing.Color.White;
+            this.choiceRed.Location = new System.Drawing.Point(203, 424);
+            this.choiceRed.Name = "choiceRed";
+            this.choiceRed.Size = new System.Drawing.Size(250, 13);
+            this.choiceRed.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::chooseyourOwnAdventure.Properties.Resources._8a79ae60c65c025c65d1e9a70fdacd95;
+            this.ClientSize = new System.Drawing.Size(870, 455);
+            this.Controls.Add(this.choiceRed);
+            this.Controls.Add(this.choiceYellow);
+            this.Controls.Add(this.choiceBlue);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.pictureBox8);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.b);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.Label choiceBlue;
+        private System.Windows.Forms.Label choiceYellow;
+        private System.Windows.Forms.Label choiceRed;
+    }
+}
+
---
+++ chooseyourOwnAdventure/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chooseyourOwnAdventure
+{
+    public partial class Form1 : Form
+    {
+        int scene = 1;
+        Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void b(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.B)
+            {
+                if (scene == 1) { scene = 11; }
+                else if (scene == 2) { scene = 2; }
+                else if (scene == 3) { scene = 20; }
+                else if (scene == 4) { scene = 5; }
+                else if (scene == 5) { scene = 21; }
+                else if (scene == 6) { scene = 21; }
+                else if (scene == 7) { scene = 21; }
+                else if (scene == 8) { scene = 20; }
+                else if (scene == 9) { scene = 6; }
+                else if (scene == 10) { scene = 20; }
+                else if (scene == 11) { scene = 16; }
+                else if (scene == 12)
+                {
+                    int chance = rnd.Next(1, 11);
+                    if (chance == 1)
+                    {
+                        scene = 14;
+                    }
+                    else
+                    {
+                        scene = 13;
+                    }
+                }
+                else if (scene == 13) { scene = 20; }
+                else if (scene == 14) { scene = 20; }
+                else if (scene == 15) { scene = 20; }
+                else if (scene == 16) { scene = 16; }
+                else if (scene == 18) { scene = 20; }
+                else if (scene == 19) { scene = 20; }
+                else if (scene == 21) { scene = 20; }
+            }
+            else if (e.KeyCode == Keys.N)
+            {
+                if (scene == 1) { scene = 2; }
+                else if (scene == 2) { scene = 3; }
+                else if (scene == 4) { scene = 9; }
+                else if (scene == 5) { scene = 6; }
+                else if (scene == 6) { scene = 7; }
+                else if (scene == 7) { scene = 8; }
+                else if (scene == 9) { scene = 15; }
+                else if (scene == 11) { scene = 15; }
+                else if (scene == 12)
+                {
+                    int chance = rnd.Next(1, 11);
+                    if (chance == 1)
+                    {
+                        scene = 14;
+                    }
+                    else
+                    {
+                        scene = 13;
+                    }
+                }
+                else if (scene == 16) { scene = 17; }
+                else if (scene == 17) { scene = 18; }
+            }
+            else if (e.KeyCode == Keys.M)
+            {
+                if (scene == 1) { scene = 1; }
+                else if (scene == 2) { scene = 4; }
+                else if (scene == 4) { scene = 15; }
+                else if (scene == 9) { scene = 10; }
+                else if (scene == 11) { scene = 12; }
+                else if (scene == 12)
+                {
+                    int chance = rnd.Next(1, 11);
+                    if (chance == 1)
+                    {
+                        scene = 14;
+                    }
+                    else
+                    {
+                        scene = 13;
+                    }
+                }
+                else if (scene == 16) { scene = 15; }
+                else if (scene == 17) { scene = 19; }
+            }
+
+            switch (scene)
+            {
+                case 1:
+                    outputLabel.Text = "You wake up in an unfamiliar place. you're shackled to the wall, and you see a small figure glaring at you from the shadows";
+                    choiceBlue.Text = "Freak out";
+                    choiceYellow.Text = "Question the figure";
+                    choiceRed.Text = "Go back to sleep";
+                    break;
+                case 2:
+                    outputLabel.Text = "you ask the figure 'why am i here?' it does not respond.";
+                    choiceBlue.Text = "Ask again";
+                    choiceYellow.Text = "Give them the silent treatment";
+                    choiceRed.Text = "cry";
+                    break;
+                case 3:
+                    outputLabel.Text = "You remain silent, in an attempt to goad them into talking to you. You are so petty that you remain silent for the rest of your life.";
+                    choiceBlue.Text = "Game Over";
+                    choiceRed.Text = "";
+                    choiceYellow.Text = "";
+                    break;
+                case 4:
+                    outputLabel.Text = "you begin to cry. The figure begins to walk out of the shadows, its... Tom Cruise??!?!?!??";
+                    choiceBlue.Text = "Keep crying";
+                    choiceYellow.Text = "Stop Crying";
+                    choiceRed.Text = "Mock him for his height";
+                    break;
+
+
+            }
+        }
+
+
+
+
+    }
+}
